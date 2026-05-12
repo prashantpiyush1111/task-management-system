dc0af1c85a0988c47929ffbf206d418cc30abc70
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-13 00:21:14 +0530

    fix: BoardCard lazy loading fix

--- src/main/java/com/TaskManagement/Entity/BoardCard.java
+++ src/main/java/com/TaskManagement/Entity/BoardCard.java
@@ -1,10 +1,10 @@
 package com.TaskManagement.Entity;
-
+ 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+ 
 import jakarta.persistence.*;
 import lombok.*;
-
+ 
 @Entity
 @Table(name = "board_cards", indexes = { @Index(columnList = "board_id,column_id,position") })
 @Data
@@ -12,18 +12,19 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class BoardCard {
-
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+ 
 	private Long boardId;
 	private Long issueId;
-
-	@ManyToOne(fetch = FetchType.LAZY)
+ 
+	@ManyToOne(fetch = FetchType.EAGER)  
 	@JoinColumn(name = "column_id")
 	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	private BoardColumn column;
-
+ 
 	private Integer position;
 }
+ 
