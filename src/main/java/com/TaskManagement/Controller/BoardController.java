98a4ee3eb0bf1c8f4cbf4413658c9c978b38c538
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-12 19:51:40 +0530

    fix(board-controller): return 404 when board is not found instead of empty optional response

--- src/main/java/com/TaskManagement/Controller/BoardController.java
+++ src/main/java/com/TaskManagement/Controller/BoardController.java
@@ -2,9 +2,7 @@ package com.TaskManagement.Controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
