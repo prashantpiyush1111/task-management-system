c10ac15649ec66655342f6d144ba747d561b5274
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-13 00:24:42 +0530

    fix: add @FeignClient annotation and fix @PathVariable in UserClient

--- src/main/java/com/TaskManagement/Client/UserClient.java
+++ src/main/java/com/TaskManagement/Client/UserClient.java
@@ -1,15 +1,17 @@
-
 package com.TaskManagement.Client;
-
+ 
 import java.util.Set;
-
+ 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
+ 
 import com.TaskManagement.Enum.Role;
-
+ 
+@FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserClient {
-	@GetMapping("api/users/{email}/roles")
-	Set<Role> getRoles(@RequestParam String officialEmail);
-
+ 
+	@GetMapping("/api/users/{email}/roles")
+	Set<Role> getRoles(@PathVariable("email") String officialEmail);
 }
